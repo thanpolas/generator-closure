e39d92eecc09882dc847553af1ff6db89406bcda
Thanasis Polychronakis <thanpolas@gmail.com>
2013-03-16 19:15:51 +0200

    digging...

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var generator = require('yeoman-generator');
 var util      = require('util');
+var path      = require('path');
 
 // Documentation: https://github.com/yeoman/generator/wiki/base
 
@@ -7,6 +8,9 @@ var Generator = module.exports = function Generator() {
   generator.Base.apply(this, arguments);
   // this.option('flag', { desc: 'Desc for flag', ...})
   // this.argument('filename', { desc: 'Desc for filename argument', ...})
+
+  this.hookFor('webapp', { args:['--compassBootstrap'] });
+
 };
 
 util.inherits(Generator, generator.Base);
@@ -15,4 +19,18 @@ util.inherits(Generator, generator.Base);
 // templates/ root) to the specified location
 Generator.prototype.scaffold = function scaffold() {
   this.directory('.', 'html');
+
+  this.sourceRoot(path.join(__dirname, '../templates'));
+
+  this.log.writeln('Uhhhh duh');
+
+  // this.directory('js');
+  // this.copy('index.html', 'index.html');
+
+
+  // var gen = this.invoke('webapp', {
+  //   compassBootstrap: true,
+  //   includeRequireJS: true
+  // });
+
 };
