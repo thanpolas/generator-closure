d6233a90ddf4130b09a312d73566d1f170a1662e
Thanasis Polychronakis <thanpolas@gmail.com>
2013-04-14 18:44:21 +0300

    prepping for next release

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,21 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    release: {
+      options: {
+        bump: true, //default: true
+        file: 'package.json', //default: package.json
+        add: true, //default: true
+        commit: true, //default: true
+        tag: true, //default: true
+        push: true, //default: true
+        pushTags: true, //default: true
+        npm: true, //default: true
+        tagName: 'v<%= version %>', //default: '<%= version %>'
+        commitMessage: 'releasing v<%= version %>', //default: 'release <%= version %>'
+        tagMessage: 'v<%= version %>' //default: 'Version <%= version %>'
+      }
+    }
   });
 
 };
